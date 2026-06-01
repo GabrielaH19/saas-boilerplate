85ff40bf7d1755d23caf820a88eabf51edcb93c1
Gabriela <gabry.h@outlook.com>
2026-06-01 18:00:25 +0200

    fix: middleware redirect reset password

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const mode = searchParams.get("mode");
+  const oobCode = searchParams.get("oobCode");
+
+  if (mode === "resetPassword" && oobCode) {
+    return NextResponse.redirect(
+      new URL(`/reset-password?oobCode=${oobCode}`, req.url)
+    );
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/",
+};
